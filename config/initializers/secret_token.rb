1d7474eeb97a3294e88d9a46facade746cbcda72
naoya <mine@mine-studio.com>
2013-11-04 01:48:22 +0900

    Railsのひな形の作成

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kaomoji::Application.config.secret_key_base = 'e315ca188ff2a9b4782b1de04b7205d548e0996bcec13de8822fcc2eff0f0f86a3a6de915d82fac6ce876e3a64b1edd47fb167383c15d3b5270ebbcc5d40b051'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kaomoji::Application.config.session_store :cookie_store, key: '_kaomoji_session'
