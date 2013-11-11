bc21ddd20d4609f1683369fd864d3202247562e1
naoya <mine@mine-studio.com>
2013-11-12 01:32:55 +0900

    表示画面の強化

--- app/models/kaomoji.rb
+++ app/models/kaomoji.rb
@@ -2,4 +2,17 @@ class Kaomoji < ActiveRecord::Base
   has_and_belongs_to_many :feelings
   has_and_belongs_to_many :readings
   validates_uniqueness_of :kaomoji
+
+  def status_string
+    case status
+    when 0
+      return '公開'
+    when 1
+      return '承認待ち'
+    when 2
+      return '拒否'
+    else
+      return 'エラー'
+    end
+  end
 end
